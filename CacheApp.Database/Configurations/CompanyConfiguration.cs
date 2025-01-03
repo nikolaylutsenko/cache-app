26aad53f621658e0ee54179bc13788c11869ac88
Mykola Lutsenko <nikolayilutsenko@gmail.com>
2025-01-03 15:08:56 +0200

    add company database model and configuration for it

---
+++ CacheApp.Database/Configurations/CompanyConfiguration.cs
@@ -0,0 +1,20 @@
+﻿namespace CacheApp.Database.Configurations;
+
+using Enteties;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+public class CompanyConfiguration : IEntityTypeConfiguration<Company>
+{
+    public void Configure(EntityTypeBuilder<Company> builder)
+    {
+        builder.ToTable("Companies");
+        builder.HasKey(t => t.Id);
+
+        builder.Property(t => t.Id).IsRequired();
+        builder.Property(t => t.Name).HasMaxLength(300).IsRequired();
+        builder.Property(t => t.Country).HasMaxLength(100).IsRequired();
+        builder.Property(t => t.Address).HasMaxLength(500).IsRequired();
+        builder.Property(t => t.ContactInfo).HasMaxLength(300).IsRequired();
+    }
+}
---
+++ CacheApp.Database/Enteties/Company.cs
@@ -0,0 +1,10 @@
+﻿namespace CacheApp.Database.Enteties;
+
+public class Company
+{
+    public Guid Id { get; set; }
+    public required string Name { get; set; }
+    public required string Country { get; set; }
+    public required string Address { get; set; }
+    public required string ContactInfo { get; set; }
+}
