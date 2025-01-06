d08af9907cfbc690e80f689666d2ccabe559f1e2
Mykola Lutsenko <nikolayilutsenko@gmail.com>
2025-01-06 08:23:21 +0200

    add initial migration

---
+++ CacheApp.Migrations/Migrations/20250106062016_InitialCreate.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CacheApp.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Companies",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(300)", maxLength: 300, nullable: false),
+                    Country = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    Address = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: true),
+                    ContactInfo = table.Column<string>(type: "character varying(300)", maxLength: 300, nullable: true),
+                    Version = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Companies", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tags",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(300)", maxLength: 300, nullable: false),
+                    Version = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tags", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Medication",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(300)", maxLength: 300, nullable: false),
+                    Description = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: true),
+                    ManufacturerId = table.Column<Guid>(type: "uuid", nullable: true),
+                    Version = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Medication", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Medication_Companies_ManufacturerId",
+                        column: x => x.ManufacturerId,
+                        principalTable: "Companies",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Substances",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(300)", maxLength: 300, nullable: false),
+                    Description = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: true),
+                    Formula = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: true),
+                    ManufacturerId = table.Column<Guid>(type: "uuid", nullable: true),
+                    Version = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Substances", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Substances_Companies_ManufacturerId",
+                        column: x => x.ManufacturerId,
+                        principalTable: "Companies",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Medication_Tags",
+                columns: table => new
+                {
+                    TagId = table.Column<Guid>(type: "uuid", nullable: false),
+                    MedicineId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Medication_Tags", x => new { x.TagId, x.MedicineId });
+                    table.ForeignKey(
+                        name: "FK_Medication_Tags_Medication_MedicineId",
+                        column: x => x.MedicineId,
+                        principalTable: "Medication",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Medication_Tags_Tags_TagId",
+                        column: x => x.TagId,
+                        principalTable: "Tags",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MedicineSpecification",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Form = table.Column<int>(type: "integer", nullable: true),
+                    Dosage = table.Column<string>(type: "text", nullable: true),
+                    MedicineId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MedicineSpecification", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MedicineSpecification_Medication_MedicineId",
+                        column: x => x.MedicineId,
+                        principalTable: "Medication",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ingredients",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    MedicineId = table.Column<Guid>(type: "uuid", nullable: false),
+                    SubstanceId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Quantity = table.Column<decimal>(type: "numeric", nullable: false),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ingredients", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Ingredients_Medication_MedicineId",
+                        column: x => x.MedicineId,
+                        principalTable: "Medication",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Ingredients_Substances_SubstanceId",
+                        column: x => x.SubstanceId,
+                        principalTable: "Substances",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Companies_Name",
+                table: "Companies",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ingredients_MedicineId",
+                table: "Ingredients",
+                column: "MedicineId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ingredients_SubstanceId",
+                table: "Ingredients",
+                column: "SubstanceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Medication_ManufacturerId",
+                table: "Medication",
+                column: "ManufacturerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Medication_Name",
+                table: "Medication",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Medication_Tags_MedicineId",
+                table: "Medication_Tags",
+                column: "MedicineId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MedicineSpecification_MedicineId",
+                table: "MedicineSpecification",
+                column: "MedicineId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Substances_ManufacturerId",
+                table: "Substances",
+                column: "ManufacturerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Substances_Name",
+                table: "Substances",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tags_Name",
+                table: "Tags",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ingredients");
+
+            migrationBuilder.DropTable(
+                name: "Medication_Tags");
+
+            migrationBuilder.DropTable(
+                name: "MedicineSpecification");
+
+            migrationBuilder.DropTable(
+                name: "Substances");
+
+            migrationBuilder.DropTable(
+                name: "Tags");
+
+            migrationBuilder.DropTable(
+                name: "Medication");
+
+            migrationBuilder.DropTable(
+                name: "Companies");
+        }
+    }
+}
