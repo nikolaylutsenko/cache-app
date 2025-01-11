f4c348f1a3166fe75e737b4b8f08604056d1b862
Mykola Lutsenko <mykola.lutsenko@dataart.com>
2025-01-11 15:50:26 +0200

    adjust result realization

--- CacheApp.Utils/ResultPattern/Result.cs
+++ CacheApp.Utils/ResultPattern/Result.cs
@@ -1,34 +1,46 @@
-﻿namespace CacheApp.Utils.ResultPattern;
+﻿using System.Diagnostics.Contracts;
 
-public class Result<TValue>
+namespace CacheApp.Utils.ResultPattern;
+
+// adjusted according to https://devblogs.microsoft.com/ise/next-level-clean-architecture-boilerplate/
+public struct Result<T>
 {
-    public TValue? Value { get; }
+    private enum ResultState
+    {
+        Null,
+        Failure,
+        Success,
+    }
+
+    public T? Value { get; }
     public Error? Error { get; }
 
     private bool _isSuccess;
+    private readonly ResultState _state;
+    public bool IsSuccess => _state == ResultState.Success;
+    public bool IsFailure => _state == ResultState.Failure;
+    public bool IsNull => _state == ResultState.Null;
 
-    private Result(TValue value)
+    private Result(T value)
     {
-        _isSuccess = true;
         Value = value;
         this.Error = default;
+        _state = ResultState.Success;
     }
 
     private Result(Error error)
     {
-        _isSuccess = false;
         Value = default;
         this.Error = error;
+        _state = ResultState.Failure;
     }
 
-    public static implicit operator Result<TValue>(TValue value) => new(value);
+    public static implicit operator Result<T>(T? value) =>
+        value is not null ? new Result<T>(value) : new Result<T>();
 
-    public static implicit operator Result<TValue>(Error error) => new(error);
+    public static implicit operator Result<T>(Error error) => new(error);
 
-    public Result<TValue> Match(
-        Func<TValue, Result<TValue>> success,
-        Func<Error, Result<TValue>> failure
-    )
+    public Result<T> Match(Func<T, Result<T>> success, Func<Error, Result<T>> failure)
     {
         if (_isSuccess)
         {
@@ -36,4 +48,17 @@ public class Result<TValue>
         }
         return failure(this.Error!);
     }
+
+    [Pure]
+    public TR Match<TR>(
+        Func<T, TR> onSuccess,
+        Func<Error, TR> onFailure,
+        Func<TR>? onNull = null
+    ) =>
+        IsSuccess ? onSuccess(Value!)
+        : IsFailure ? onFailure(Error!)
+        : onNull is not null ? onNull()
+        : throw new InvalidOperationException(
+            "Result is null, but no onNull function was provided."
+        );
 }
