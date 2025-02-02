8fe525c71c3b1ab8ea195ed41fd19e36d93d1557
Mykola Lutsenko <mykola.lutsenko@dataart.com>
2025-02-02 11:38:27 +0200

    add factory method

---
+++ CacheApp.Utils/GOFPatterns/FactoryMethod/FactoryMethod.cs
@@ -0,0 +1,36 @@
+﻿namespace CacheApp.Utils.GOFPatterns.FactoryMethod;
+
+// applicability
+// 1. when you don't know beforehand the exact types and dependencies of the object your code should work with
+// 2. when you want to provide a way to extend components
+// 3. save system resources by reusing existing objects instead of rebuilding them each time (singleton?)
+
+public class FactoryMethod
+{
+    // move general items into interface
+    public interface IProduct
+    {
+        string Name { get; }
+    }
+
+    // create concrete realization
+    public class ProductA : IProduct
+    {
+        public string Name => "Product A";
+
+        // some other functionality
+    }
+
+    // abstract creator that will have also some additional general logic
+    public interface ICreator<T>
+        where T : IProduct
+    {
+        T CreateProduct();
+    }
+
+    // concrete creator also with some specific for this type logic
+    public class CreatorA : ICreator<ProductA>
+    {
+        public ProductA CreateProduct() => new();
+    }
+}
