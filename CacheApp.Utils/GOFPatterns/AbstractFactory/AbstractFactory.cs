747ce756ea6fadeb885a33566baa64a17edf7288
Mykola Lutsenko <mykola.lutsenko@dataart.com>
2025-02-02 11:58:31 +0200

    add abstract factory

---
+++ CacheApp.Utils/GOFPatterns/AbstractFactory/AbstractFactory.cs
@@ -0,0 +1,78 @@
+﻿namespace CacheApp.Utils.GOFPatterns.AbstractFactory;
+
+// advanced factory method, works well with family of related products
+
+public class AbstractFactory
+{
+    public interface IProductA
+    {
+        string Name { get; }
+    }
+
+    public interface IProductB
+    {
+        string Name { get; }
+    }
+
+    public class ProductA1 : IProductA
+    {
+        public string Name => "Product A 1";
+
+        // some other functionality
+    }
+
+    // create concrete realization
+    public class ProductB1 : IProductB
+    {
+        public string Name => "Product B 1";
+
+        // some other functionality
+    }
+
+    public class ProductA2 : IProductA
+    {
+        public string Name => "Product A 2";
+
+        // some other functionality
+    }
+
+    // create concrete realization
+    public class ProductB2 : IProductB
+    {
+        public string Name => "Product B 2";
+
+        // some other functionality
+    }
+
+    public interface ICreator
+    {
+        IProductA CreateProductA();
+        IProductB CreateProductB();
+    }
+
+    public class CreatorA1 : ICreator
+    {
+        public IProductA CreateProductA()
+        {
+            return new ProductA1();
+        }
+
+        public IProductB CreateProductB()
+        {
+            return new ProductB1();
+        }
+    }
+
+    public class CreatorA2 : ICreator
+    {
+        public IProductA CreateProductA()
+        {
+            return new ProductA1();
+        }
+
+        public IProductB CreateProductB()
+        {
+            return new ProductB1();
+        }
+    }
+}
