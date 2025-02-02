9fecd4959aadaacf06d61f565bf39c51072760e0
Mykola Lutsenko <mykola.lutsenko@dataart.com>
2025-02-02 12:33:01 +0200

    add prototype

---
+++ CacheApp.Utils/GOFPatterns/Prototype/Prototype.cs
@@ -0,0 +1,27 @@
+﻿namespace CacheApp.Utils.GOFPatterns.Prototype;
+
+// moves logic of cloning inside the object
+// so objects create copies of themselves
+
+// or maybe used instead of inheritance for creation of objects family
+
+public interface IPrototype<T>
+{
+    T Clone();
+}
+
+public class ProductA(string Category) : IPrototype<ProductA>
+{
+    public ProductA Clone()
+    {
+        return new ProductA(Category);
+    }
+}
+
+public class ProductB(string Name, decimal Price, string Category) : ProductA(Category)
+{
+    public new ProductB Clone()
+    {
+        return new ProductB(Name, Price, Category);
+    }
+}
